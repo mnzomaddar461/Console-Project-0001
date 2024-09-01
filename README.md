ad9263279117217eb4cf17159fab648ecbc95b44
mnzomaddar461 <mrnaimzomaddar461@gmail.com>
2024-09-02 05:35:53 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Console-Project-0001
---
+++ quize_game.c
@@ -0,0 +1,436 @@
+#include<stdio.h>
+#include<conio.h>
+#include <stdlib.h>
+int main(){
+  int choice;
+  float sum = 0,cor=0,wro=0;
+
+  printf("\t\tProject Name: Quize Of C Programming\n");
+  printf("\t\tDevloper Group: Bengal Programmer\n");
+
+  printf("I) Which one  is Escape Sequence for The (Alert)Bell?\n");
+  printf("1) O       2) T\n");
+  printf("3) V       4) A\n");
+  printf("Please Enter The Asnwer: ");
+  scanf("%d", &choice);
+  if (choice==4)
+  {
+    printf("Your Answer is Correct!\n");
+    sum += 1;
+    cor += 1;
+  }
+  else
+  {
+    printf("Ohh ! You are wrong\n");
+    sum -= 0.25;
+    wro += 1;
+  }
+  printf("Hit Enter For Next Question");
+  getch();
+  system("cls");
+    //End Of Question
+  printf("II) How Many Keywords in C Programming?\n");
+  printf("1) 69       2) 420\n");
+  printf("3) 32       4) 23\n");
+  printf("Please Enter The Asnwer: ");
+  scanf("%d", &choice);
+  if (choice==3)
+  {
+    printf("Your Answer is Correct!\n");
+    sum += 1;
+    cor += 1;
+  }
+  else
+  {
+    printf("Ohh ! You are wrong\n");
+    sum -= 0.25;
+    wro += 1;
+  }
+  printf("Hit Enter For Next Question");
+  getch();
+  system("cls");
+    //End Of Question
+  printf("III) What the size of Int?\n");
+  printf("1) 4 Bytes       2) 16 Bytes\n");
+  printf("3) 8 Bytes       4) 7 Bytes\n");
+  printf("Please Enter The Asnwer: ");
+  scanf("%d", &choice);
+  if (choice==1)
+  {
+    printf("Your Answer is Correct!\n");
+    sum += 1;
+    cor += 1;
+  }
+  else
+  {
+    printf("Ohh ! You are wrong\n");
+    sum -= 0.25;
+    wro += 1;
+  }
+  printf("Hit Enter For Next Question");
+  getch();
+  system("cls");
+    //End Of Question
+  printf("IV) Which One Is Input Function?\n");
+  printf("1) scanf       2) prinft\n");
+  printf("3) puts       4) getchar\n");
+  printf("Please Enter The Asnwer: ");
+  scanf("%d", &choice);
+  if (choice==1)
+  {
+    printf("Your Answer is Correct!\n");
+    sum += 1;
+    cor += 1;
+  }
+  else
+  {
+    printf("Ohh ! You are wrong\n");
+    sum -= 0.25;
+    wro += 1;
+  }
+  printf("Hit Enter For Next Question");
+  getch();
+  system("cls");
+  //End Of Question
+  printf("V) Which Arithmetic Operator used for get Square Root of a number?\n");
+  printf("1) pow       2) tan\n");
+  printf("3) cos       4) sqrt\n");
+  printf("Please Enter The Asnwer: ");
+  scanf("%d", &choice);
+  if (choice==4)
+  {
+    printf("Your Answer is Correct!\n");
+    sum += 1;
+    cor += 1;
+  }
+  else
+  {
+    printf("Ohh ! You are wrong\n");
+    sum -= 0.25;
+    wro += 1;
+  }
+  printf("Hit Enter For Next Question");
+  getch();
+  system("cls");
+  //End Of Question
+  printf("VI) Which One Is Correct Variable?\n");
+  printf("1) NumBer       2) number\n");
+  printf("3) num ber       4) 2number\n");
+  printf("Please Enter The Asnwer: ");
+  scanf("%d", &choice);
+  if (choice==2)
+  {
+    printf("Your Answer is Correct!\n");
+    sum += 1;
+    cor += 1;
+  }
+  else
+  {
+    printf("Ohh ! You are wrong\n");
+    sum -= 0.25;
+    wro += 1;
+  }
+  printf("Hit Enter For Next Question");
+  getch();
+  system("cls");
+  //End Of Question
+  printf("VII) What the size of char?\n");
+  printf("1) 1 Bits       2) 8 Bytes\n");
+  printf("3) 1 Biytes       4) 8 Bits\n");
+  printf("Please Enter The Asnwer: ");
+  scanf("%d", &choice);
+  if (choice==4)
+  {
+    printf("Your Answer is Correct!\n");
+    sum += 1;
+    cor += 1;
+  }
+  else
+  {
+    printf("Ohh ! You are wrong\n");
+    sum -= 0.25;
+    wro += 1;
+  }
+  printf("Hit Enter For Next Question");
+  getch();
+  system("cls");
+  //End Of Question
+  printf("VIII) Which One Is Increment for Unary Operator?\n");
+  printf("1) +a-       2) +a+\n");
+  printf("3) a++       4) ++a\n");
+  printf("Please Enter The Asnwer: ");
+  scanf("%d", &choice);
+  if (choice==3)
+  {
+    printf("Your Answer is Correct!\n");
+    sum += 1;
+    cor += 1;
+  }
+  else
+  {
+    printf("Ohh ! You are wrong\n");
+    sum -= 0.25;
+    wro += 1;
+  }
+  printf("Hit Enter For Next Question");
+  getch();
+  system("cls");
+  //End Of Question
+  printf("XI) Which one Is Logical Not in Logical Operator?\n");
+  printf("1) !       2) ?\n");
+  printf("3) &       4) ||\n");
+  printf("Please Enter The Asnwer: ");
+  scanf("%d", &choice);
+  if (choice==1)
+  {
+    printf("Your Answer is Correct!\n");
+    sum += 1;
+    cor += 1;
+  }
+  else
+  {
+    printf("Ohh ! You are wrong\n");
+    sum -= 0.25;
+    wro += 1;
+  }
+  printf("Hit Enter For Next Question");
+  getch();
+  system("cls");
+  //End Of Question
+  printf("X) What the size of Double?\n");
+  printf("1) 8 Bits       2) 64 Bits\n");
+  printf("3) 64 Bytes       4) 8 Biytes\n");
+  printf("Please Enter The Asnwer: ");
+  scanf("%d", &choice);
+  if (choice==2)
+  {
+    printf("Your Answer is Correct!\n");
+    sum += 1;
+    cor += 1;
+  }
+  else
+  {
+    printf("Ohh ! You are wrong\n");
+    sum -= 0.25;
+    wro += 1;
+  }
+  printf("Hit Enter For Next Question");
+  getch();
+  system("cls");
+  //End Of Question
+  printf("XI) Which One is correct?\n");
+  printf("1) pRiNtF()       2) PriNtF(""):\n");
+  printf("3) printf("");       4) printf()\n");
+  printf("Please Enter The Asnwer: ");
+  scanf("%d", &choice);
+  if (choice==3)
+  {
+    printf("Your Answer is Correct!\n");
+    sum += 1;
+    cor += 1;
+  }
+  else
+  {
+    printf("Ohh ! You are wrong\n");
+    sum -= 0.25;
+    wro += 1;
+  }
+  printf("Hit Enter For Next Question");
+  getch();
+  system("cls");
+  //End Of Question
+  printf("XII) Which Is The ASCII for capital 'X'?\n");
+  printf("1) 99        2) 69\n");
+  printf("3) 120       4) 95\n");
+  printf("Please Enter The Asnwer: ");
+  scanf("%d", &choice);
+  if (choice==4)
+  {
+    printf("Your Answer is Correct!\n");
+    sum += 1;
+    cor += 1;
+  }
+  else
+  {
+    printf("Ohh ! You are wrong\n");
+    sum -= 0.25;
+    wro += 1;
+  }
+  printf("Hit Enter For Next Question");
+  getch();
+  system("cls");
+  //End Of Question
+  printf("XIII) What is The Format Spceifier for 'Double?\n");
+  printf("1) %%d       2) %%lf \n");
+  printf("3) %%l       4) %%f\n");
+  printf("Please Enter The Asnwer: ");
+  scanf("%d", &choice);
+  if (choice==2)
+  {
+    printf("Your Answer is Correct!\n");
+    sum += 1;
+    cor += 1;
+  }
+  else
+  {
+    printf("Ohh ! You are wrong\n");
+    sum -= 0.25;
+    wro += 1;
+  }
+  printf("Hit Enter For Next Question");
+  getch();
+  system("cls");
+//End Of Question
+  printf("XVI) Which One Is Bitwize ExOR?\n");
+  printf("1) &       2) ^\n");
+  printf("3) ~       4) >>\n");
+  printf("Please Enter The Asnwer: ");
+  scanf("%d", &choice);
+  if (choice==2)
+  {
+    printf("Your Answer is Correct!\n");
+    sum += 1;
+    cor += 1;
+  }
+  else
+  {
+    printf("Ohh ! You are wrong\n");
+    sum -= 0.25;
+    wro += 1;
+  }
+  printf("Hit Enter For Next Question");
+  getch();
+  system("cls");
+//End Of Question
+  printf("XV) Which Symbol Use For Trenary Operator?\n");
+  printf("1) num1>num2 ? num1::num2       2) num1>num2 ? num1:num2\n");
+  printf("3) num1>num2 ?? num1:num2       4) num1>>num2 ?? num1::num2\n");
+  printf("Please Enter The Asnwer: ");
+  scanf("%d", &choice);
+  if (choice==2)
+  {
+    printf("Your Answer is Correct!\n");
+    sum += 1;
+    cor += 1;
+  }
+  else
+  {
+    printf("Ohh ! You are wrong\n");
+    sum -= 0.25;
+    wro += 1;
+  }
+  printf("Hit Enter For Next Question");
+  getch();
+  system("cls");
+//End Of Question
+  printf("XVI) Printf (\"Sum is 69\") Here is the correct Output\n");
+  printf("1) sum=69          2) 69\n");
+  printf("3) sum is 69       4) Sum Is 69\n");
+  printf("Please Enter The Asnwer: ");
+  scanf("%d", &choice);
+  if (choice==4)
+  {
+    printf("Your Answer is Correct!\n");
+    sum += 1;
+    cor += 1;
+  }
+  else
+  {
+    printf("Ohh ! You are wrong\n");
+    sum -= 0.25;
+    wro += 1;
+  }
+  printf("Hit Enter For Next Question");
+  getch();
+  system("cls");
+//End Of Question
+printf("XVII) Which one is loop?\n");
+  printf("1) or       2) for\n");
+  printf("3) else if       4) if\n");
+  printf("Please Enter The Asnwer: ");
+  scanf("%d", &choice);
+  if (choice==2)
+  {
+    printf("Your Answer is Correct!\n");
+    sum += 1;
+    cor += 1;
+  }
+  else
+  {
+    printf("Ohh ! You are wrong\n");
+    sum -= 0.25;
+    wro += 1;
+  }
+  printf("Hit Enter For Next Question");
+  getch();
+  system("cls");
+//End Of Question
+printf("XVIII) What to use for conditional statement?\n");
+  printf("1) if        2) while\n");
+  printf("3) for       4) do while\n");
+  printf("Please Enter The Asnwer: ");
+  scanf("%d", &choice);
+  if (choice==1)
+  {
+    printf("Your Answer is Correct!\n");
+    sum += 1;
+    cor += 1;
+  }
+  else
+  {
+    printf("Ohh ! You are wrong\n");
+    sum -= 0.25;
+    wro += 1;
+  }
+  printf("Hit Enter For Next Question");
+  getch();
+  system("cls");
+//End Of Question
+printf("XIX) In C programm flowchart the shape Rectengular what's mean?\n");
+  printf("1) Processing        2) Start T\n");
+  printf("3) Take Input       4) Addition A\n");
+  printf("Please Enter The Asnwer: ");
+  scanf("%d", &choice);
+  if (choice==1)
+  {
+    printf("Your Answer is Correct!\n");
+    sum += 1;
+    cor += 1;
+  }
+  else
+  {
+    printf("Ohh ! You are wrong\n");
+    sum -= 0.25;
+    wro += 1;
+  }
+  printf("Hit Enter For Next Question");
+  getch();
+  system("cls");
+//End Of Question
+  printf("XX) Who is the inventor of C Programming\n");
+  printf("1) Nur Musk      2) Naim Thomas\n");
+  printf("3) Tom Holland       4) Dennis Ritchie\n");
+  printf("Please Enter The Asnwer: ");
+  scanf("%d", &choice);
+  if (choice==4)
+  {
+    printf("Your Answer is Correct!\n");
+    sum += 1;
+    cor += 1;
+  }
+  else
+  {
+    printf("Ohh ! You are wrong\n");
+    sum -= 0.25;
+    wro += 1;
+  }
+  printf("Hit Enter For Next Question");
+  getch();
+  system("cls");
+//End Of Question
+  //SuM Of Total Marks
+  printf("You Have Give %.1f right answer\n",cor);
+  printf("But You Have Guessed %.1f wrong\n",wro);
+  printf("Your Total marks is%.1f - %.1f = %.1f \n",cor,wro,sum);
+  return 0;
+}
